30604fdef075f7e6e78ec8e65d4b82d951b94a74
Jon Garfunkel <jongarfunkel@gmaik.com>
2026-04-13 20:22:36 -0400

    setup default keys in the env

---
+++ server/env-defaults.ts
@@ -0,0 +1,4 @@
+// Set default values for process.env keys here.
+process.env.PORT = process.env.PORT ?? "5000";
+process.env.OPENAI_API_KEY = process.env.OPENAI_API_KEY ?? "no key";
+process.env.PINECONE_API_KEY = process.env.PINECONE_API_KEY ?? "no key";
